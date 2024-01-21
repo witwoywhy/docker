67da3c3e3a6bfb621e60e92964c5f30cff31e836
witwoywhy <but.nuttawit.dev@gmail.com>
2024-01-21 16:17:52 +0700

    gitlab ce

---
+++ gitlab-ce.up.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+docker run -d \
+    --publish 8081:80 --publish 2222:22 \
+    --name gitlab \
+    --restart always \
+    --volume $GITLAB_HOME/config:/etc/gitlab \
+    --volume $GITLAB_HOME/logs:/var/log/gitlab \
+    --volume $GITLAB_HOME/data:/var/opt/gitlab \
+    gitlab/gitlab-ce:latest
+
+
+# docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
